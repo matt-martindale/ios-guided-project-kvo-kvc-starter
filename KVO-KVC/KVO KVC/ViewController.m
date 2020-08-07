b4b9db7586412d9d3693873d049f42b3074bf1ba
Matt Martindale <martindalematt18@yahoo.com>
2020-08-06 19:45:14 -0600

    Quick introduction to accessing the value for a given key

--- KVO-KVC/KVO KVC/ViewController.m
+++ KVO-KVC/KVO KVC/ViewController.m
@@ -26,10 +26,10 @@
 
     LSIDepartment *marketing = [[LSIDepartment alloc] init];
     marketing.name = @"Marketing";
-    LSIEmployee * philSchiller = [[LSIEmployee alloc] init];
+    LSIEmployee *philSchiller = [[LSIEmployee alloc] init];
     philSchiller.name = @"Phil";
     philSchiller.jobTitle = @"VP of Marketing";
-    philSchiller.salary = 10000000; 
+    philSchiller.salary = 10000000;
     marketing.manager = philSchiller;
 
     
@@ -67,6 +67,15 @@
     
     NSLog(@"%@", self.hrController);
     
+    NSString *key = @"jobTitle";
+    
+    NSString *value = [craig valueForKey:key];
+    NSLog(@"value for key %@: %@", key, value);
+    
+    for (id employee in engineering.employees) {
+        NSString *value = [employee valueForKey:key];
+        NSLog(@"value for key %@: %@", key, value);
+    }
     
 }
 
